6123aff4222aba83e4d171cb0c27d0475732b57d
aliter230880 <56067954+aliter230880@users.noreply.github.com>
2026-04-09 20:20:29 +0300

    Update Identity.sol

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -7,7 +7,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol"; 
 
 /**
  * @title Identity
