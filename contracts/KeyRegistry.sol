67595c18373331ca3f017c3f44b2e352af597804
aliter230880 <56067954+aliter230880@users.noreply.github.com>
2026-04-11 20:22:37 +0300

    Update KeyRegistry.sol

--- contracts/KeyRegistry.sol
+++ contracts/KeyRegistry.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
+contract KeyRegistry {
+    mapping(address => bytes) public publicKeys;
+
+    event PublicKeySet(address indexed user, bytes publicKey);
+
+    function setPublicKey(bytes calldata publicKey) external {
+        require(publicKey.length > 0, "Empty key");
+        publicKeys[msg.sender] = publicKey;
+        emit PublicKeySet(msg.sender, publicKey);
+    }
+
+    function getPublicKey(address user) external view returns (bytes memory) {
+        return publicKeys[user];
+    }
+}
